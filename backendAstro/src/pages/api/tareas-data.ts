2211518806af769763ac973bf2dddd51c854cf2c
AgustinaWaigel <soledad.waigel@uap.edu.ar>
2025-04-25 13:18:19 -0300

    manejo de ajax

--- backendAstro/src/pages/api/tareas-data.ts
+++
@@ -1,9 +0,0 @@
-import type { APIRoute } from "astro";
-import { getTareas } from "../lib/tareas"; // Ajustá el path si cambia
-
-export const GET: APIRoute = async () => {
-  const tareas = getTareas();
-  return new Response(JSON.stringify(tareas), {
-    headers: { "Content-Type": "application/json" },
-  });
-};
