b8dc0c2da45c90c6fe8e0b9c199de2ad03c91ac4
Nuria Desteffani <nuria.desteffani@uap.edu.ar>
2025-04-27 04:55:51 -0300

    Revisar problemas con los filtros - El codigo utiliza tailwind

--- Semana 5/src/lib/requestParse.ts
+++ Semana 5/src/lib/requestParse.ts
@@ -1,19 +1,18 @@
 export const parseFormData = async (request: Request) => {
-    const formData = await request.formData();
-    const text = formData.get("task");
-    const id = formData.get("task-id");
-    const done = formData.get("done");
-    return { text, id, done };
+  const formData = await request.formData();
+  const text = formData.get("task");
+  const id = formData.get("task-id");
+  const done = formData.get("done");
+  return { text, id, done };
+};
+
+export const parseJson = async (
+  request: Request
+): Promise<{ text?: string; id?: string; done?: boolean }> => {
+  const { text, id, done } = await request.json();
+  return {
+    text: typeof text === "string" ? text : undefined,
+    id: typeof id === "string" ? id : undefined,
+    done: typeof done === "boolean" ? done : undefined,
   };
-  
-  export const parseJson = async (
-    request: Request
-  ): Promise<{ text?: string; id?: number; done?: boolean }> => {
-    const { text, id, done } = await request.json();
-    return {
-      text: typeof text === "string" ? text : undefined,
-      id: typeof id === "number" ? id : undefined,
-      done: typeof done === "boolean" ? done : undefined,
-    };
-  };
-  
+};
