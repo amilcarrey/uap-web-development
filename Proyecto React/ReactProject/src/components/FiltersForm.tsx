9e8114e01d4c0d44460410359bc1615e24c39042
Nuria Desteffani <nuria.desteffani@uap.edu.ar>
2025-05-14 01:16:00 -0300

    Primer commit del TP1 de React: Se supone que la tarea estaria completa

---
+++ Proyecto React/ReactProject/src/components/FiltersForm.tsx
@@ -0,0 +1,23 @@
+type FiltersFormProps = {
+  filter: "all" | "completed" | "incomplete";
+  setFilter: (value: "all" | "completed" | "incomplete") => void;
+}
+
+export function FiltersForm({ filter, setFilter }: FiltersFormProps) {
+  return (
+    <form method="GET" className="flex justify-center gap-2 mt-5" name="filter-form">
+      <button type="submit" className="px-4 py-2 bg-[#b07c7c] rounded-[5px] text-white text-[16px] cursor-pointer" name="filter" value="all" onClick={(e) => {
+        e.preventDefault()
+        setFilter("all")
+      }}>All</button>
+      <button type="submit" className="px-4 py-2 bg-[#b07c7c] rounded-[5px] text-white text-[16px] cursor-pointer" name="filter" value="incomplete" onClick={(e) => {
+        e.preventDefault()
+        setFilter("incomplete")
+      }}>Incomplete</button>
+      <button type="submit" className="px-4 py-2 bg-[#b07c7c] rounded-[5px] text-white text-[16px] cursor-pointer" name="filter" value="completed" onClick={(e) => {
+        e.preventDefault()
+        setFilter("completed")
+      }}>Completed</button>
+    </form>
+  );
+}
