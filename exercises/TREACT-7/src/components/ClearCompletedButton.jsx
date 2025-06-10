c31d1fc7b0171155e515cdf00f562ae2f954fcde
Nicolás Cerdá <nico.gabriel02@yahoo.com.ar>
2025-06-10 17:55:24 -0300

    lesto1

--- exercises/TREACT-7/src/components/ClearCompletedButton.jsx
+++ exercises/TREACT-7/src/components/ClearCompletedButton.jsx
@@ -8,7 +8,7 @@ export default function ClearCompletedButton({ onClear }) {
         onClick={onClear}
         className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
       >
-        Limpiar Completadas
+        Limpiar Completadas!!
       </button>
     </div>
   );
