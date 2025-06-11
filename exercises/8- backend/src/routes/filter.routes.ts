eedc61b9ccc4f2214b10c30f1d71ee6dbae5a7ad
andrewblockernst <andrewblockernst@gmail.com>
2025-06-10 22:18:34 -0300

    connection between backend express and frontend

--- exercises/8- backend/src/routes/filter.routes.ts
+++ exercises/8- backend/src/routes/filter.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { FilterController } from "../modules/filter/filter.controller";
+import { authenticateToken } from "../middleware/auth.middleware";
 
 const router = Router();
 const filterController = new FilterController();
+
+// All filter routes require authentication
+router.use(authenticateToken);
 
 // Filter routes
 router.get("/", filterController.getFilter.bind(filterController));
