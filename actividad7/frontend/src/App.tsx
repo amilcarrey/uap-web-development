214fbfdf18261c32b0f20f89d2269afc6ecdcfcd
Noemi <noemi.fernandez@uap.edu.ar>
2025-06-25 14:00:27 -0300

    actualizacion

---
+++ actividad7/frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-center text-2xl font-bold text-gray-700">
+      Gestor de tareas base
+    </div>
+  );
+}
+
+export default App;
--- actividad7/frontend/src/services/tasksApi.ts
+++
@@ -1,7 +0,0 @@
-// src/services/tasksApi.ts
-export const getTasks = async () => {
-  return [
-    { id: 1, text: 'Aprender Zustand', completed: false },
-    { id: 2, text: 'Usar React Query', completed: true },
-  ];
-};
