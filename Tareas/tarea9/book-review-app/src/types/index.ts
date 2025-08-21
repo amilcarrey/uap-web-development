fecb898a7c8b1cc27aca6e72da4326191b11be73
Beskow Jones Johann Natán <johann.beskow@uap.edu.ar>
2025-08-20 23:56:03 -0300

    en proceso

---
+++ Tareas/tarea9/book-review-app/src/types/index.ts
@@ -0,0 +1,12 @@
+export type GoogleBook = {
+  id: string;
+  volumeInfo: {
+    title: string;
+    authors?: string[];
+    description?: string;
+    imageLinks?: {
+      thumbnail?: string;
+    };
+    publishedDate?: string;
+  };
+};
