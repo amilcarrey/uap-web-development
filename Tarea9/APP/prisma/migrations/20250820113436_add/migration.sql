bf55c38c9209add2b81a7ff0fb6bef9036926654
HarrysonLadines <Harrysonladines@gmail.com>
2025-09-03 23:47:43 -0300

    .

--- Tarea9/APP/prisma/migrations/20250820113436_add/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "public"."Reseña" DROP CONSTRAINT "Reseña_usuarioId_fkey";
-
--- AlterTable
-ALTER TABLE "public"."Reseña" ALTER COLUMN "usuarioId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "public"."Reseña" ADD CONSTRAINT "Reseña_usuarioId_fkey" FOREIGN KEY ("usuarioId") REFERENCES "public"."Usuario"("id") ON DELETE SET NULL ON UPDATE CASCADE;
